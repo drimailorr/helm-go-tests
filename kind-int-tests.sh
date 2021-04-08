4e6597d3628731b409242845ae25276132a64187
Roman Drimailo <drimailorr@gmail.com>
2021-04-08 13:23:23 +0300

    cells

--- kind-int-tests.sh
+++ kind-int-tests.sh
@@ -1,6 +1,9 @@
 
+#%%
+
 kind delete clusters primary
 
+#%%
 cat <<EOF | kind create cluster --wait 180s --name "primary" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -32,9 +35,10 @@ nodes:
 #  image: kindest/node:v1.18.8
 EOF
 
-
+#%%
 kubectl config set current-context kind-primary
 
+#%%
 # Install ingress
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 #kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s
@@ -42,17 +46,25 @@ kubectl config set current-context kind-primary
 # Install Calico (disableDefaultCNI: true)
 # curl https://docs.projectcalico.org/manifests/calico.yaml | kubectl apply -f -
 
+#%%
 docker build --tag go-tests -f test/Dockerfile.test .
 
+#%%
 # kind load dosen't recognize existing docker layers therefore very slow
 # This looks clunky atm until better UI implemented: https://kind.sigs.k8s.io/docs/user/local-registry/
 # hostPath volumes seem like a better way to decouple actual tests from docker image
+
+#%%
 kind load docker-image go-tests --name primary
 
+#%%
 kubectl delete job go-tests --namespace go-tests
 
+#%%
 kubectl apply -f test/go-tests.yaml --namespace go-tests
 
+#%%
 kubectl wait --namespace go-tests --for=condition=complete job/go-tests --timeout=300s
 
+#%%
 kubectl --namespace go-tests logs -l type=go-tests --tail=1500 -f
